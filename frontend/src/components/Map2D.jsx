e3b85d52dd66346312dcfee9613e4c55054f9e36
stephanetchatchum@gmail.com <stephanetchatchum@gmail.com>
2026-07-15 21:13:15 +0200

    damn

--- frontend/src/components/Map2D.jsx
+++ frontend/src/components/Map2D.jsx
@@ -172,7 +172,7 @@ function Map2D({
 
         // Tune these until nodes line up perfectly
         const NODE_TRANSFORM = {
-        'Social Commons-2': { scaleX: 0.7, scaleY: 0.68, offsetX: -4, offsetY: 95 },
+        'Social Commons-2': { scaleX: 0, scaleY: 0, offsetX: 0, offsetY: 100 },
         'Social Commons-1': { scaleX: 0.61, scaleY: 0.65, offsetX: 45, offsetY: -25 },
         'Social Commons-0': { scaleX: 0.6, scaleY: 0.6, offsetX: 45, offsetY: 0 },
         }
